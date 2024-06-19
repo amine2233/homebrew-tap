2ef1b4539bf2eeb841830242271babed797222b9
Amine Bensalah <amine.bensalah@intech-consulting.fr>
2024-06-19 07:32:01 +0200

    feat: upgrade to version 0.6.2

--- Casks/passapplication.rb
+++ Casks/passapplication.rb
@@ -2,8 +2,8 @@ cask 'passapplication' do
     depends_on formula: 'pass'
     depends_on macos: '>= :ventura'
 
-    version '0.6.0-rc-1'
-    sha256 '8d610326fd2cb005ceea742241312d239610b447833a82426d082630af286509'
+    version '0.6.2'
+    sha256 '8dcae60528ecf57e8536acb3e21c944b1f2e0e1226c0060aff0523a866384ee0'
     url "https://github.com/amine2233/password-store-app/releases/download/#{version}/PassApplication.app.zip"
     name 'PassApplication'
     homepage 'https://github.com/amine2233/password-store-app'
